cd1ef51aac3bf39dec6f1214ffdf7c0f5a642f80
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-10-04 17:15:58 -0300

    Exercicio prático - tarde

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function adicionaItem(event){
     container.insertAdjacentElement('beforeend',addItem)
     addItem.classList.add("item")
     addItem.onclick = (evento) => removeItem(evento)
-
 }
 
 
